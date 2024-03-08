75aa94424111139bec87987d019df3597610cb85
Kennedy Mac <kennedy.adams@dcmail.ca>
2024-03-08 09:41:06 -0500

    added post style blocks

--- plugins/mindfulink-core-block-hooks/plugin.js
+++ plugins/mindfulink-core-block-hooks/plugin.js
@@ -1,94 +1,119 @@
-
-
-wp.blocks.registerBlockStyle('core/heading', {
-    name: 'heading',
-    label: 'Heading',
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "heading",
+	label: "Heading",
 });
 
-wp.blocks.registerBlockStyle('core/heading', {
-    name: 'sub-heading',
-    label: 'Sub Heading',
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "sub-heading",
+	label: "Sub Heading",
 });
 
-wp.blocks.registerBlockStyle('core/pullquote', {
-    name: 'pullquote',
-    label: 'Pullquote',
+wp.blocks.registerBlockStyle("core/pullquote", {
+	name: "pullquote",
+	label: "Pullquote",
 });
 
-wp.blocks.registerBlockStyle('core/paragraph', {
-    name: 'paragraph',
-    label: 'Paragraph',
+wp.blocks.registerBlockStyle("core/paragraph", {
+	name: "paragraph",
+	label: "Paragraph",
 });
 
-wp.blocks.registerBlockStyle('core/quote', {
-    name: 'quote',
-    label: 'Quote',
+wp.blocks.registerBlockStyle("core/quote", {
+	name: "quote",
+	label: "Quote",
 });
 
-wp.blocks.registerBlockStyle('core/list', {
-    name: 'list',
-    label: 'List',
+wp.blocks.registerBlockStyle("core/list", {
+	name: "list",
+	label: "List",
 });
 
-wp.blocks.registerBlockStyle('core/table', {
-    name: 'table',
-    label: 'Table',
+wp.blocks.registerBlockStyle("core/table", {
+	name: "table",
+	label: "Table",
 });
 
-wp.blocks.registerBlockStyle('core/footnotes', {
-    name: 'footnotes',
-    label: 'Footnotes',
+wp.blocks.registerBlockStyle("core/footnotes", {
+	name: "footnotes",
+	label: "Footnotes",
 });
 
-wp.blocks.registerBlockStyle('core/verse', {
-    name: 'verse',
-    label: 'Verse',
+wp.blocks.registerBlockStyle("core/verse", {
+	name: "verse",
+	label: "Verse",
 });
 
-wp.blocks.registerBlockStyle('core/details', {
-    name: 'details',
-    label: 'Details',
+wp.blocks.registerBlockStyle("core/details", {
+	name: "details",
+	label: "Details",
 });
 
-wp.blocks.registerBlockStyle('core/preformatted', {
-    name: 'preformatted',
-    label: 'Preformatted',
+wp.blocks.registerBlockStyle("core/preformatted", {
+	name: "preformatted",
+	label: "Preformatted",
 });
 
-wp.blocks.registerBlockStyle('core/code', {
-    name: 'code',
-    label: 'Code',
+wp.blocks.registerBlockStyle("core/code", {
+	name: "code",
+	label: "Code",
 });
 
-
-wp.blocks.registerBlockStyle('core/group', {
-    name: 'card-lavender',
-    label: 'Lavender Card',
+wp.blocks.registerBlockStyle("core/group", {
+	name: "card-lavender",
+	label: "Lavender Card",
 });
 
-
-wp.blocks.registerBlockStyle('core/group', {
-    name: 'card-blue',
-    label: 'Blue Card',
+wp.blocks.registerBlockStyle("core/group", {
+	name: "card-blue",
+	label: "Blue Card",
 });
 
-
-wp.blocks.registerBlockStyle('core/group', {
-    name: 'card-green',
-    label: 'Green Card',
+wp.blocks.registerBlockStyle("core/group", {
+	name: "card-green",
+	label: "Green Card",
 });
 
-wp.blocks.registerBlockStyle('core/group', {
-    name: 'card-gold',
-    label: 'Gold Card',
+wp.blocks.registerBlockStyle("core/group", {
+	name: "card-gold",
+	label: "Gold Card",
 });
 
-wp.blocks.registerBlockStyle('core/group', {
-    name: 'card-taupe',
-    label: 'Taupe Card',
+wp.blocks.registerBlockStyle("core/group", {
+	name: "card-taupe",
+	label: "Taupe Card",
 });
 
-wp.blocks.registerBlockStyle('core/group', {
-    name: 'header-row',
-    label: 'Header Row',
+wp.blocks.registerBlockStyle("core/group", {
+	name: "header-row",
+	label: "Header Row",
+});
+
+wp.blocks.registerBlockStyle("core/post-featured-image", {
+	name: "featured-image",
+	label: "Featured Image",
+});
+
+wp.blocks.registerBlockStyle("core/query-pagination-previous", {
+	name: "previous-post",
+	label: "Previous Post",
+});
+
+wp.blocks.registerBlockStyle("core/query-pagination-next", {
+	name: "next-post",
+	label: "Next Post",
+});
+
+wp.blocks.registerBlockStyle("core/post-template", {
+	name: "post-template",
+	label: "Post Template",
+});
+
+wp.blocks.registerBlockStyle("core/post-template", {
+	name: "post-template",
+	label: "Post Template",
+});
+
+wp.blocks.registerBlockStyle("core/post-title", {
+	name: "post-title",
+	label: "Post Title",
 });
